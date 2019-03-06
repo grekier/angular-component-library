25711205fda5c905e46ea5a7c90704478a946c01
Geoffrey Rekier <geoffrey.rekier@gjensidige.no>
2019-03-06 15:46:04 +0100

    Starting with ng file drop

--- projects/dnd-with-preview/src/lib/dnd-with-preview.component.ts
+++ projects/dnd-with-preview/src/lib/dnd-with-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FileSystemDirectoryEntry, FileSystemFileEntry, UploadEvent, UploadFile} from 'ngx-file-drop';
 
 @Component({
   selector: 'osnode-dnd-with-preview',
@@ -10,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class DndWithPreviewComponent implements OnInit {
+  public files: File[] = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public dropped(event: UploadEvent) {
+    for (const droppedFile of event.files) {
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+          console.log(droppedFile.relativePath, file);
+          this.files.push(file);
+        });
+      } else {
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+        // TODO add all files recursively
+      }
+    }
+  }
+
+  public fileOver(event) {
+    console.log(event);
+  }
+
+  public fileLeave(event) {
+    console.log(event);
+  }
 }
--- projects/dnd-with-preview/src/lib/dnd-with-preview.module.ts
+++ projects/dnd-with-preview/src/lib/dnd-with-preview.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { DndWithPreviewComponent } from './dnd-with-preview.component';
+import { FileDropModule } from 'ngx-file-drop';
 
 @NgModule({
   declarations: [DndWithPreviewComponent],
   imports: [
+    FileDropModule
   ],
   exports: [DndWithPreviewComponent]
 })
